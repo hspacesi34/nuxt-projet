47c68e62cc7711b387ecf52062c60748d898d178
hspacesi34 <107484761+hspacesi34@users.noreply.github.com>
2025-11-05 16:56:03 +0100

    feat: ajout .env

--- app/services/firebase.js
+++ app/services/firebase.js
@@ -1,12 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAmMkVZkzeZsvthmStpLg0gONPjIIR3wrE",
+  apiKey: process.env.API_KEY,
   authDomain: "nuxt-tp-d308e.firebaseapp.com",
   databaseURL: "https://nuxt-tp-d308e-default-rtdb.firebaseio.com",
   projectId: "nuxt-tp-d308e",
